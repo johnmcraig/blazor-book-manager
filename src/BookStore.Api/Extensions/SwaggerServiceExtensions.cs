ba80208af61b288e51240a475437dcab08714a67
johnmcraig <jmcraig32884@gmail.com>
2020-07-26 19:25:41 -0400

    Updated swagger title

--- src/BookStore.Api/Extensions/SwaggerServiceExtensions.cs
+++ src/BookStore.Api/Extensions/SwaggerServiceExtensions.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BookStore.Api.Extensions
 {
@@ -14,7 +10,7 @@ namespace BookStore.Api.Extensions
         {
             services.AddSwaggerGen(options =>
             {
-                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Api", Version = "v1" });
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Book Store Api", Version = "v1" });
             });
 
             return services;
