fe9ffa7ea461ffa8a880df397b57a74dc651e845
johnmcraig <jmcraig32884@gmail.com>
2020-07-26 19:23:57 -0400

    Cleaned up using statements

--- src/BookStore.Api/Startup.cs
+++ src/BookStore.Api/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using BookStore.Api.Extensions;
+using BookStore.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
 namespace BookStore.Api
 {
@@ -24,6 +19,7 @@ namespace BookStore.Api
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddInfrastructure();
             services.AddSwaggerDocumentation();
             services.AddControllers();
         }
