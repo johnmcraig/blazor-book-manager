4f8925d2d81d3cde2fbaa3643349cd986f6ffc13
johnmcraig <jmcraig32884@gmail.com>
2020-08-05 21:54:47 -0400

    Changed base Url for dotnet cli developement launch properties

--- src/BookStore.UI.Wasm/Static/Endpoints.cs
+++ src/BookStore.UI.Wasm/Static/Endpoints.cs
@@ -7,7 +7,7 @@ namespace BookStore.UI.Wasm.Static
 {
     public static class Endpoints
     {
-        public static string BaseUrl = "https://localhost:44383";
+        public static string BaseUrl = "https://localhost:5001";
         public static string AuthorsEndpont = $"{BaseUrl}/api/authors/";
         public static string BooksEndpont = $"{BaseUrl}/api/books/";
     }
