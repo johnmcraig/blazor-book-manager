1b8fdb560ab45cdacb66a715c3db769d87e9dc24
johnmcraig <jmcraig32884@gmail.com>
2020-08-10 00:02:08 -0400

    Added navigation properties and made foriegn key nullalbe

--- src/BookStore.Core/Entities/Book.cs
+++ src/BookStore.Core/Entities/Book.cs
@@ -13,6 +13,8 @@ namespace BookStore.Core.Entities
         public string Isbn { get; set; }
         public string Image { get; set; }
         public decimal? Price { get; set; }
-        public int AuthorId { get; set; }
+
+        public int? AuthorId { get; set; }
+        public Author Author { get; set; }
     }
 }
