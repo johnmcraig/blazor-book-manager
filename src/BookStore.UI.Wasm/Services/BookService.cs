0409c09834d9a2b6b6fdecfa869e40e9d61fc7d2
John Craig <jmcraig32884@gmail.com>
2020-08-22 23:49:52 -0400

    cleaned up commented code, injected local storage

--- src/BookStore.UI.Wasm/Services/BookService.cs
+++ src/BookStore.UI.Wasm/Services/BookService.cs
@@ -1,4 +1,5 @@
-﻿using BookStore.UI.Wasm.Contracts;
+﻿using Blazored.LocalStorage;
+using BookStore.UI.Wasm.Contracts;
 using BookStore.UI.Wasm.Models;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,18 +14,13 @@ namespace BookStore.UI.Wasm.Services
     {
         private readonly HttpClient _client;
         private readonly ILogger<BookService> _logger;
-        //private readonly ILocalStorageService _localStorage;
+        private readonly ILocalStorageService _localStorage;
 
-        public BookService(HttpClient client, ILogger<BookService> logger) : base(client, logger)
+        public BookService(HttpClient client, ILogger<BookService> logger, ILocalStorageService localStorage) : base(client, logger, localStorage)
         {
             _logger = logger;
             _client = client;
-            // , ILocalStorageService localStorage _localStorage = localStorage;
+            _localStorage = localStorage;
         }
-
-        //private async Task<string> GetBearerToken()
-        //{
-        //    return await _localStorage.GetItemAsync<string>("authToken");
-        //}
     }
 }
