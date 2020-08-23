5667eeaea7181fc5fe9a0f1b4d524289ad917666
John Craig <jmcraig32884@gmail.com>
2020-08-22 23:48:39 -0400

    Changed update method to task of bool

--- src/BookStore.UI.Wasm/Contracts/IRepositoryService.cs
+++ src/BookStore.UI.Wasm/Contracts/IRepositoryService.cs
@@ -10,7 +10,7 @@ namespace BookStore.UI.Wasm.Contracts
         Task<IList<T>> GetAll(string url);
         Task<T> GetSingle(string url, int id);
         Task<T> Create(string url, T entity);
-        Task<T> Update(string url, T entity, int id);
+        Task<bool> Update(string url, T entity, int id);
         Task<bool> Delete(string url, int id);
     }
 }
