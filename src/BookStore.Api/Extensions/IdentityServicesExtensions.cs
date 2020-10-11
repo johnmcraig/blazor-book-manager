e34495cee1cecb72f84db7520f9edf353e166ad8
johnmcraig <jmcraig32884@gmail.com>
2020-10-11 10:49:35 -0400

    Created an extension class for Identity, user manager and roles, and Jwt validation tokens for the DI to Startup

---
+++ src/BookStore.Api/Extensions/IdentityServicesExtensions.cs
@@ -0,0 +1,41 @@
+﻿using BookStore.Core.Entities.Identity;
+using BookStore.Infrastructure.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace BookStore.Api.Extensions
+{
+    public static class IdentityServicesExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, 
+            IConfiguration config)
+        {
+            var builder = services.AddIdentityCore<AppUser>();
+
+            builder = new IdentityBuilder(builder.UserType, typeof(AppRole), builder.Services);
+            builder.AddEntityFrameworkStores<AppIdentityDbContext>();
+            builder.AddSignInManager<SignInManager<AppUser>>();
+            builder.AddRoleValidator<RoleValidator<AppRole>>();
+            builder.AddRoleManager<RoleManager<AppRole>>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Token:Key"])),
+                        ValidIssuer = config["Token:Issuer"],
+                        ValidateIssuer = true,
+                        ValidateAudience = false
+                    };
+                });
+
+            return services;
+        }
+    }
+}
