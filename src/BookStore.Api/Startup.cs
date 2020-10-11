15548d393822be512bedcaea3f89666bf2641501
johnmcraig <jmcraig32884@gmail.com>
2020-10-11 10:42:43 -0400

    Refactore constructor for configurations. Added Identity services

--- src/BookStore.Api/Startup.cs
+++ src/BookStore.Api/Startup.cs
@@ -2,6 +2,7 @@ using BookStore.Api.Extensions;
 using BookStore.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,16 +11,17 @@ namespace BookStore.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        private readonly IConfiguration _config;
 
-        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddInfrastructure();
+            services.AddIdentityServices(_config);
 
             services.AddCors(opt =>
             {
