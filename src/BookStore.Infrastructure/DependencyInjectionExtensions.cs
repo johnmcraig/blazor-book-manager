0044d9f78677f8adfbdf19aca1674c5032ab2d11
johnmcraig <jmcraig32884@gmail.com>
2020-10-11 12:34:02 -0400

    Added Identity context class for DI and rearranged order of services

--- src/BookStore.Infrastructure/DependencyInjectionExtensions.cs
+++ src/BookStore.Infrastructure/DependencyInjectionExtensions.cs
@@ -2,6 +2,7 @@
 using BookStore.Core.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using BookStore.Infrastructure.DataAccess;
+using BookStore.Infrastructure.Identity;
 
 namespace BookStore.Infrastructure
 {
@@ -10,14 +11,15 @@ namespace BookStore.Infrastructure
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
             services.AddScoped<ISqlDataAccess, SqliteDataAccess>();
-            services.AddDbContext<StoreContext>();
 
+            services.AddDbContext<StoreContext>();
+            services.AddDbContext<AppIdentityDbContext>();
+
+
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
             services.AddScoped<IBookRepository, BookSqlRepository>();
             services.AddScoped<IAuthorRepository, AuthorSqlRepository>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
-
             services.AddSingleton<ILoggerService, LoggerService>();
 
             return services;
