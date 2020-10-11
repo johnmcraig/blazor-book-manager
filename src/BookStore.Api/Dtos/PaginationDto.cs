fc1b5065837c3e35d1b82f7d3e3c46a4959f0df6
johnmcraig <jmcraig32884@gmail.com>
2020-10-11 19:09:20 -0400

    Created a Dto for pagination mapping

---
+++ src/BookStore.Api/Dtos/PaginationDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Api.Dtos
+{
+    public class PaginationDto
+    {
+        public int Page { get; set; } = 1;
+        public int QuantityPerPage { get; set; } = 10;
+    }
+}
