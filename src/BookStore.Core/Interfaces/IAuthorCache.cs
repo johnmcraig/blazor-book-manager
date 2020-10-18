d852a0f751d72670a849804c9e8a4377321a819b
johnmcraig <jmcraig32884@gmail.com>
2020-10-18 11:12:22 -0400

    Created interface abstraction for author memory caching and concrete implementation

---
+++ src/BookStore.Core/Interfaces/IAuthorCache.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Core.Interfaces
+{
+    public interface IAuthorCache
+    {
+        Author Get(int id);
+        void Remove(int id);
+        void Set(Author author);
+    }
+}
---
+++ src/BookStore.Infrastructure/Services/AuthorCache.cs
@@ -0,0 +1,42 @@
+﻿using BookStore.Core.Entities;
+using BookStore.Core.Interfaces;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Infrastructure.Services
+{
+    public class AuthorCache : IAuthorCache
+    {
+        private MemoryCache _cache { get; set; }
+
+        private string GetCacheKey(int id) => $"Author-{id}";
+
+        public AuthorCache()
+        {
+            _cache = new MemoryCache(new MemoryCacheOptions
+            {
+                SizeLimit = 100
+            });
+        }
+
+        public Author Get(int id)
+        {
+            _cache.TryGetValue(GetCacheKey(id), out Author author);
+            return author;
+        }
+
+        public void Remove(int id)
+        {
+            _cache.Remove(GetCacheKey(id));
+        }
+
+        public void Set(Author author)
+        {
+            var cacheEntryOptions = new MemoryCacheEntryOptions().SetSize(1);
+            _cache.Set(GetCacheKey(author.Id), author, cacheEntryOptions);
+        }
+
+    }
+}
