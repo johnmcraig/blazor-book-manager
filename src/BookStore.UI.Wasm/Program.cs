fe1c373e8841d448d0068cb7f4d05b5a7eece801
John Craig <jmcraig32884@gmail.com>
2020-10-25 21:38:47 -0400

    Cleaned up unused statements

--- src/BookStore.UI.Wasm/Program.cs
+++ src/BookStore.UI.Wasm/Program.cs
@@ -1,14 +1,10 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using BookStore.UI.Wasm.Contracts;
 using BookStore.UI.Wasm.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Blazored.LocalStorage;
 using Blazored.Toast;
 
